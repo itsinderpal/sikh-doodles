4d229f80ba972aa9a31926baa7f3ff726d2a72ea
Inderpal Singh <inderpalsingh6763@gmail.com>
2022-01-26 17:59:56 -0500

    Deployment Fix#2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-import SikhDoodles from "./artifacts/SikhDoodles.json";
+import SikhDoodles from "./SikhDoodles.json";
 import Main from "./components/Main";
 import { formatUnits } from "ethers/lib/utils";
 
